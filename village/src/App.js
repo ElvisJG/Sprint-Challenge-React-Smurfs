640b0511e01bd4b488e76757d7245b6bc7e0096d
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-28 19:15:34 -0400

    axios get complete, instructions are... weird lol

--- village/src/App.js
+++ village/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
@@ -8,15 +9,28 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
+
+  componentDidMount() {
+    Axios.get('http://localhost:3333/smurfs')
+      .then(res => {
+        this.setState({
+          smurfs: res.data
+        });
+      })
+      .catch(err => {
+        console.log('Error:', err);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <SmurfForm />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
